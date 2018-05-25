5025a07c1f5dce09067370f2b02d4a01f6c46c5b
samanth <ss@edvenswa.in>
2018-05-25 18:50:20 +0530

    no message

---
+++ src/main/java/com/controller/Project.java
@@ -0,0 +1,12 @@
+package com.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project.class, args);
+	}
+}
